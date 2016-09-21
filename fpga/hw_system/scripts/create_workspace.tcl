678914635fc4bc9f333d0391d6dd687733d8c246
Vaxi Atrea Ramnit <xavie.arteaga@gmail.com>
2016-09-21 21:37:04 +0200

    Create workspace TCL script updated

--- fpga/hw_system/scripts/create_workspace.tcl
+++ fpga/hw_system/scripts/create_workspace.tcl
@@ -12,21 +12,21 @@ sdk create_hw_project -name hw_0 -hwspec output/hw_system.hdf
 sdk create_bsp_project -name bsp_0 -hwproject hw_0 -mss src/system.mss
 
 # 4) Create Bootloader BSP
-sdk create_bsp_project -name srec_spi_bootloader_bsp -hwproject hw_0 -mss src/bootloader_bsp.mss
+#sdk create_bsp_project -name srec_spi_bootloader_bsp -hwproject hw_0 -mss src/bootloader_bsp.mss
 
 # 5) Create Hello World Project
 sdk create_app_project -name helloworld -hwproject hw_0 -proc microblaze_0 -os standalone -lang C -app {Hello World} -bsp bsp_0
 
 # 6) Create Bootloader application
-sdk create_app_project -name srec_spi_bootloader -hwproject hw_0 -proc microblaze_0 -os standalone -lang C -app {SREC SPI Bootloader} -bsp srec_spi_bootloader_bsp
+#sdk create_app_project -name srec_spi_bootloader -hwproject hw_0 -proc microblaze_0 -os standalone -lang C -app {SREC SPI Bootloader} -bsp srec_spi_bootloader_bsp
 
 # 7) Modify bootloader pointer
-exec echo "#undef FLASH_IMAGE_BASEADDR" >> ../sw_system/sdk_workspace/srec_spi_bootloader/src/blconfig.h
-exec echo "#define FLASH_IMAGE_BASEADDR 0x400000" >> ../sw_system/sdk_workspace/srec_spi_bootloader/src/blconfig.h
+#exec echo "#undef FLASH_IMAGE_BASEADDR" >> ../sw_system/sdk_workspace/srec_spi_bootloader/src/blconfig.h
+#exec echo "#define FLASH_IMAGE_BASEADDR 0x400000" >> ../sw_system/sdk_workspace/srec_spi_bootloader/src/blconfig.h
 
 # 8) Compile all projects
 sdk build_project bsp_0
-sdk build_project srec_spi_bootloader_bsp
+#sdk build_project srec_spi_bootloader_bsp
 sdk build_project helloworld
-sdk build_project srec_spi_bootloader
+#sdk build_project srec_spi_bootloader
 exit
