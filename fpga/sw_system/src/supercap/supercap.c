ed559e23bdc0c3cecfd485053919728b2a97e0a2
vaxi <xavie.arteaga@gmail.com>
2017-05-06 17:48:24 +0200

    Supercap component refactored

--- fpga/sw_system/src/supercap/supercap.c
+++ fpga/sw_system/src/supercap/supercap.c
@@ -18,6 +18,7 @@ static void supercap_update(uint32_t elapsed);
 static uint8_t supercap_percent = 0;
 static uint16_t supercap_charge = 0;
 static uint16_t supercap_measure = 0;
+static supercap_state_t supercap_state = SUPERCAP_STATE_CRITICAL_UNKNOWN;
 static scheduler_entry_t supercap_update_entry = {0, PLATFORM_SUPERCAP_UPDATE_PERIOD, supercap_update};
 
 #ifdef PLATFORM_SUPERCAP_MONITOR_PERIOD
@@ -27,8 +28,8 @@ void supercap_monitor(uint32_t elapsed);
 static scheduler_entry_t supercap_monitor_entry = {0, PLATFORM_SUPERCAP_MONITOR_PERIOD, supercap_monitor};
 
 void supercap_monitor(uint32_t elapsed) {
-    LOG("Percent: %3d %%; Charge: %4d mV; Measure: %4d mV", (int) supercap_percent, (int) supercap_charge,
-        (int) supercap_measure);
+    LOG("Percent: %3d %%; Charge: %4d mV; Measure: %4d mV; Status: %s", (int) supercap_percent, (int) supercap_charge,
+        (int) supercap_measure, SUPERCAP_STATE_TOSTR(supercap_state));
 }
 
 #endif /* PLATFORM_SUPERCAP_MONITOR_PERIOD */
@@ -62,11 +63,18 @@ static void supercap_update(uint32_t elapsed) {
     supercap_charge = supercap_measure * (int16_t) PLATFORM_SUPERCAP_VOLTAGE_SCALE;
     supercap_percent = (uint8_t) ((100 * supercap_charge) / PLATFORM_SUPERCAP_VOLTAGE_MAX);
 
-    if (supercap_percent > PLATFORM_SUPERCAP_THREASHOLD_GREEN) {
-        leds_turn_on(LEDS_CHAN_GREEN);
-        leds_turn_off(LEDS_CHAN_RED);
+    if (supercap_percent > PLATFORM_SUPERCAP_THREASHOLD_BLUE) {
+        leds_turn_on_timeout(LEDS_CHAN_BLUE, PLATFORM_SUPERCAP_LED_TIMEOUT);
+        supercap_state = SUPERCAP_STATE_CHARGED;
+    }else if (supercap_percent > PLATFORM_SUPERCAP_THREASHOLD_GREEN) {
+        leds_turn_on_timeout(LEDS_CHAN_GREEN, PLATFORM_SUPERCAP_LED_TIMEOUT);
+        supercap_state = SUPERCAP_STATE_LOW_CHARGE;
     } else {
-        leds_turn_off(LEDS_CHAN_GREEN);
-        leds_turn_on(LEDS_CHAN_RED);
+        leds_turn_on_timeout(LEDS_CHAN_RED, PLATFORM_SUPERCAP_LED_TIMEOUT);
+        supercap_state = SUPERCAP_STATE_CRITICAL_CHARGE;
     }
+}
+
+supercap_state_t supercap_read_status (void) {
+    return supercap_state;
 }
--- fpga/sw_system/src/supercap/supercap.h
+++ fpga/sw_system/src/supercap/supercap.h
@@ -2,6 +2,19 @@
 #ifndef SW_SYSTEM_SUPERCAP_H
 #define SW_SYSTEM_SUPERCAP_H
 
+#define SUPERCAP_STATE_TOSTR(X) ((const char*)(X==SUPERCAP_STATE_CRITICAL_CHARGE) ?\
+                    "critical charge":(X==SUPERCAP_STATE_LOW_CHARGE)? \
+                    "low charge":(X==SUPERCAP_STATE_CHARGED)? \
+                    "charged":"unkwown")
+
+typedef enum {
+    SUPERCAP_STATE_CRITICAL_UNKNOWN = -1,
+    SUPERCAP_STATE_CRITICAL_CHARGE = 0,
+    SUPERCAP_STATE_LOW_CHARGE = 1,
+    SUPERCAP_STATE_CHARGED = 2
+} supercap_state_t;
+
 int supercap_init (void);
+supercap_state_t supercap_read_status (void);
 
 #endif //SW_SYSTEM_SUPERCAP_H
